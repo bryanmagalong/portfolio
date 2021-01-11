10feb964a1125b81d13908b9e0d6faec223044b1
Bryan M <allen.bry95@gmail.com>
2021-01-11 22:45:30 +0100

    feat: add ContentWrapper component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Navbar from './components/Navbar';
-
+import ContentWrapper from './components/ContentWrapper';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ContentWrapper />
     </div>
   );
 }
