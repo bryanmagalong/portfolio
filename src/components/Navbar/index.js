3707567a0c7bb5bf0f1e31a323db669c34cb2fef
Bryan M <allen.bry95@gmail.com>
2021-01-11 22:34:58 +0100

    feat(Navbar): add dropdown menu on small screens and better responsive behavior

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
 const Navbar = () => (
-  <nav className="w-full flex justify-between items-center px-navbar h-navbar bg-primary-dark text-white">
+  <div className="navbar lg:navbar-lg w-full flex flex-wrap lg:flex-nowrap justify-between items-center bg-primary-dark text-white">
     <a href="#home">Magalong</a>
-    <div>Collapse</div>
-  </nav>
+    <label for="menu-toggle" className="lg:hidden block">
+      Menu
+    </label>
+    <input
+      className="hidden checked:drop-visible"
+      type="checkbox"
+      id="menu-toggle"
+    />
+    <div
+      className="hidden pt-dropdown w-full lg:block lg:w-auto lg:pt-0"
+      id="menu"
+    >
+      <nav>
+        <ul className="flex flex-col items-center lg:flex-row gap-y-7 lg:gap-x-50">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#skillset">Skillset</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#projects">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 );
 
 export default Navbar;
