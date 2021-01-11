c996d3555b6562cc6d987543d8b9f256ea5e6bb0
Bryan M <allen.bry95@gmail.com>
2021-01-11 18:35:03 +0100

    feat: add Navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav className="w-full flex justify-between items-center px-navbar h-navbar bg-primary-dark text-white">
+    <a href="#home">Magalong</a>
+    <div>Collapse</div>
+  </nav>
+);
+
+export default Navbar;
