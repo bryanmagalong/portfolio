7198d138a4af20d34f6bc39bb87f264ff11fcb30
Bryan M <allen.bry95@gmail.com>
2021-01-11 18:34:37 +0100

    chore(tailwind config): add custom variables(height, spacing, colors, margin)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,29 @@ module.exports = {
   purge: [ './src/**/*.{js,jsx,ts,tsx}', './public/index.html' ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      height: {
+        navbar: '3.500rem',
+      },
+      spacing: {
+        //====== x axis
+        sm: '2.063rem',
+        lg: '8.563rem',
+        navbar: '1.625rem',
+      },
+      margin: {
+        y: {
+          title: '2.188rem',
+        },
+      },
+      colors: {
+        primary: {
+          dark: '#07063A',
+          light: '#682B85',
+        },
+        secondary: '#FD6C4D',
+      },
+    },
   },
   plugins: [],
 };
