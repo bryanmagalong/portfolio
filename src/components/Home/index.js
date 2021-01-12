6beb63a9efc7cb29d571be3b6ce05115a53a86ce
Bryan M <allen.bry95@gmail.com>
2021-01-12 21:10:24 +0100

    feat(Home): add Home content and spacing on mobile device

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const Home = ({ children }) => <div>Home</div>;
+const Home = () => (
+  <div className="pt-home">
+    <div className="text-white">
+      <p className="text-lg font-bold uppercase">
+        Hello world! I'm <span className="text-secondary">Bryan Magalong</span>.
+        Welcome to my website.
+      </p>
+      <p className="text-lg mt-26">
+        I’m a <span className="text-secondary">web developer</span> focused on{' '}
+        <span className="text-secondary">front-end</span> stack but open to full
+        stack opportunities. I have a great affinity with{' '}
+        <span className="text-secondary">Javascript</span> and one of its
+        library: <span className="text-secondary">React</span>.
+      </p>
+    </div>
+  </div>
+);
 
 export default Home;
