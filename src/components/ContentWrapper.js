f717f6c3004dd7d061e95f8a91343e2e8c9bfc25
Bryan M <allen.bry95@gmail.com>
2021-01-12 22:45:42 +0100

    feat(Home): update content responsive behavior

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ContentWrapper = ({ children }) => (
-  <div className="bg-gradient-to-b from-primary-dark to-primary-light min-h-screen px-sm lg:px-lg">
+  <div className="bg-gradient-to-b from-primary-dark to-primary-light min-h-screen px-sm lg:px-lg flex flex-col justify-center">
     {children}
   </div>
 );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,21 +2,30 @@ import React from 'react';
 import homePic from './../../assets/home-pic-1.svg';
 
 const Home = () => (
-  <div className="pt-home-top pb-home-bottom">
-    <div className="text-white">
-      <p className="text-lg font-bold uppercase">
+  <div className="pt-16 lg:flex lg:gap-x-50">
+    <div className="text-white flex flex-col justify-center gap-y-26">
+      <p className="text-lg lg:text-xl font-bold uppercase">
         Hello world! I'm <span className="text-secondary">Bryan Magalong</span>.
         Welcome to my website.
       </p>
-      <p className="text-lg mt-26">
-        I’m a <span className="text-secondary">web developer</span> focused on{' '}
-        <span className="text-secondary">front-end</span> stack but open to full
-        stack opportunities. I have a great affinity with{' '}
-        <span className="text-secondary">Javascript</span> and one of its
-        library: <span className="text-secondary">React</span>.
-      </p>
+      <div className="grid md:grid-cols-3 xl:grid-cols-1">
+        <div className="md:col-span-2 md:flex md:items-center">
+          <p className="text-lg">
+            I’m a <span className="text-secondary">web developer</span> focused
+            on <span className="text-secondary">front-end</span> stack but open
+            to full stack opportunities. I have a great affinity with{' '}
+            <span className="text-secondary">Javascript</span> and one of its
+            library: <span className="text-secondary">React</span>.
+          </p>
+        </div>
+        <div className="hidden md:block xl:hidden">
+          <img src={homePic} alt="home" />
+        </div>
+      </div>
     </div>
-    <img src={homePic} className="hidden lg:block" alt="home" />
+    <div className="hidden xl:block xl:flex xl:items-center">
+      <img src={homePic} alt="home" />
+    </div>
   </div>
 );
 
