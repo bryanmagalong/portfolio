d0296386cc883bc23d34654830a0bbf4d632b704
Bryan M <allen.bry95@gmail.com>
2021-01-13 15:32:09 +0100

    feat: add skillset static data

---
+++ src/data/skillset.js
@@ -0,0 +1,41 @@
+export const skills = [
+  {
+    name: 'html',
+    learning: false,
+  },
+  {
+    name: 'css',
+    learning: false,
+  },
+  {
+    name: 'javascript',
+    learning: false,
+  },
+  {
+    name: 'react',
+    learning: false,
+  },
+  {
+    name: 'tailwindcss',
+    learning: true,
+  },
+];
+
+export const tools = [
+  {
+    name: 'vscode',
+    learning: false,
+  },
+  {
+    name: 'git',
+    learning: false,
+  },
+  {
+    name: 'adobexd',
+    learning: false,
+  },
+  {
+    name: 'postman',
+    learning: false,
+  },
+];
