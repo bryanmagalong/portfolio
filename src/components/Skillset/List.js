a8bd2ac35b9e16dfcfda8e240b0869c741332b48
Bryan M <allen.bry95@gmail.com>
2021-01-13 19:06:05 +0100

    feat: add ListItem component

--- src/components/Skillset/List.js
+++ src/components/Skillset/List.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ListItem from './ListItem';
+
 const List = ({ title, list }) => (
   <div>
     <h1 className="title lg:title-lg">{title}</h1>
     <ul className="grid grid-cols-3 xl:grid-cols-4 gap-x-sm text-white">
-      {list.map((elem) => <li key={elem.name}>{elem.name}</li>)}
+      {list.map((elem) => <ListItem key={elem.name} name={elem.name} />)}
     </ul>
   </div>
 );
---
+++ src/components/Skillset/ListItem.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { ReactComponent as HTMLIcon } from './../../assets/icons/html.svg';
+import { ReactComponent as CSSIcon } from './../../assets/icons/css.svg';
+import { ReactComponent as JSIcon } from './../../assets/icons/javascript.svg';
+import { ReactComponent as ReactIcon } from './../../assets/icons/react.svg';
+import { ReactComponent as ReduxIcon } from './../../assets/icons/redux.svg';
+import { ReactComponent as BootstrapIcon } from './../../assets/icons/bootstrap.svg';
+import { ReactComponent as TailwindCSSIcon } from './../../assets/icons/tailwindcss.svg';
+import { ReactComponent as PHPIcon } from './../../assets/icons/php.svg';
+import { ReactComponent as NodeIcon } from './../../assets/icons/nodejs.svg';
+import { ReactComponent as ExpressIcon } from './../../assets/icons/express.svg';
+import { ReactComponent as MongoDBIcon } from './../../assets/icons/mongodb.svg';
+import { ReactComponent as VSCodeIcon } from './../../assets/icons/vscode.svg';
+import { ReactComponent as GitIcon } from './../../assets/icons/git.svg';
+import { ReactComponent as AdobeXDIcon } from './../../assets/icons/adobexd.svg';
+import { ReactComponent as PostmanIcon } from './../../assets/icons/postman.svg';
+
+const ListItem = ({ name }) => {
+  const checkNameIcon = (name) => {
+    switch (name) {
+      case 'HTML':
+        return <HTMLIcon className="mx-auto" width={65} height={65} />;
+      case 'CSS':
+        return <CSSIcon className="mx-auto" width={65} height={65} />;
+      case 'Javascript':
+        return <JSIcon className="mx-auto" width={65} height={65} />;
+      case 'React':
+        return <ReactIcon className="mx-auto" width={65} height={65} />;
+      case 'Redux':
+        return <ReduxIcon className="mx-auto" width={65} height={65} />;
+      case 'Bootstrap':
+        return <BootstrapIcon className="mx-auto" width={65} height={65} />;
+      case 'TailwindCSS':
+        return <TailwindCSSIcon className="mx-auto" width={65} height={65} />;
+      case 'PHP':
+        return <PHPIcon className="mx-auto" width={65} height={65} />;
+      case 'MongoDB':
+        return <MongoDBIcon className="mx-auto" width={65} height={65} />;
+      case 'Node.js':
+        return <NodeIcon className="mx-auto" width={65} height={65} />;
+      case 'Express':
+        return <ExpressIcon className="mx-auto" width={65} height={65} />;
+      case 'VSCode':
+        return <VSCodeIcon className="mx-auto" width={65} height={65} />;
+      case 'Git':
+        return <GitIcon className="mx-auto" width={65} height={65} />;
+      case 'AdobeXD':
+        return <AdobeXDIcon className="mx-auto" width={65} height={65} />;
+      case 'Postman':
+        return <PostmanIcon className="mx-auto" width={65} height={65} />;
+      default:
+        return <HTMLIcon className="mx-auto" width={65} height={65} />;
+    }
+  };
+
+  return (
+    <li className="text-center">
+      {checkNameIcon(name)}
+      <p>{name}</p>
+    </li>
+  );
+};
+
+export default ListItem;
