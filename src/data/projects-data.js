c62006c93be5b0adeccf5da1b558373c639d1330
Bryan M <allen.bry95@gmail.com>
2021-01-15 14:27:01 +0100

    feat: add projects-data

---
+++ src/data/projects-data.js
@@ -0,0 +1,24 @@
+import chatAppImg from './../assets/projects/chat-app.svg';
+
+const projects = [
+  {
+    imgSrc: chatAppImg,
+    title: 'Project 1',
+    techList: [ 'React', 'Symfony', 'SemanticUI' ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti rerum, vitae sapiente tenetur, cupiditate dolorem cum deleniti, aperiam est reiciendis ipsa fuga molestias rem nemo id sint. Consequatur, ullam veritatis.',
+    github: '#',
+    website: '#',
+  },
+  {
+    imgSrc: chatAppImg,
+    title: 'Project 2',
+    techList: [ 'React', 'TailwindCSS' ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti rerum, vitae sapiente tenetur, cupiditate dolorem cum deleniti, aperiam est reiciendis ipsa fuga molestias rem nemo id sint. Consequatur, ullam veritatis.',
+    github: '#',
+    website: '#',
+  },
+];
+
+export default projects;
