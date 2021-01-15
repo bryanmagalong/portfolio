2cda60e930dcc91d53d64b971fa0c795bd2c70ee
Bryan M <allen.bry95@gmail.com>
2021-01-15 15:34:46 +0100

    feat(ProjectCard): add rounded-full class for image border radius

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -11,7 +11,7 @@ const ProjectCard = ({
 }) => (
   <li className="text-white grid gap-y-13 lg:grid-cols-5 lg:gap-x-50">
     <div className="lg:col-span-2">
-      <img src={imgSrc} alt={title} />
+      <img className="rounded-lg" src={imgSrc} alt={title} />
     </div>
     <div className="lg:col-span-3">
       <h2 className="text-project font-bold lg:text-lg">{title}</h2>
