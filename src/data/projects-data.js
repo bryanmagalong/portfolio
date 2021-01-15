883a7260b841ef2ad5047ccf36b58c8234e64eea
Bryan M <allen.bry95@gmail.com>
2021-01-15 15:33:54 +0100

    chore(data): update projects datas and add orinjin svg

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -1,19 +1,20 @@
 import chatAppImg from './../assets/projects/chat-app.svg';
+import orinjin from './../assets/projects/orinjin.svg';
 
 const projects = [
   {
     imgSrc: chatAppImg,
-    title: 'Project 1',
-    techList: [ 'React', 'Symfony', 'SemanticUI' ],
+    title: 'Chat App - FrontMentor',
+    techList: [ 'React', 'TailwindCSS' ],
     description:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti rerum, vitae sapiente tenetur, cupiditate dolorem cum deleniti, aperiam est reiciendis ipsa fuga molestias rem nemo id sint. Consequatur, ullam veritatis.',
     github: '#',
     website: '#',
   },
   {
-    imgSrc: chatAppImg,
-    title: 'Project 2',
-    techList: [ 'React', 'TailwindCSS' ],
+    imgSrc: orinjin,
+    title: "O'Rinjin",
+    techList: [ 'React', 'Symfony', 'SemanticUI' ],
     description:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti rerum, vitae sapiente tenetur, cupiditate dolorem cum deleniti, aperiam est reiciendis ipsa fuga molestias rem nemo id sint. Consequatur, ullam veritatis.',
     github: '#',
